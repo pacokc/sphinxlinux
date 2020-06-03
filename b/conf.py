0a265a03d89dbb4b4bb1072c96c28f1b1ef721a6
pacokc <paco@DESKTOP-88AEJ4I.localdomain>
2020-06-03 13:39:54 +0200

    asdasda

---
+++ b/conf.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Sphinx configuration file."""
+import time
+
+author = "Jan Holthuis"
+project = "sphinx-multiversion"
+release = "0.2.4"
+version = "0.2"
+copyright = "{}, {}".format(time.strftime("%Y"), author)
+
+html_last_updated_fmt = "%c"
+master_doc = "index"
+pygments_style = "friendly"
+templates_path = ["_templates"]
+extensions = [
+    "sphinx_multiversion",
+]
+
+templates_path = [
+    "_templates",
+]
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+        "versioning.html",
+    ],
+}
+
